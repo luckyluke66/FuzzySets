5109c8e66538997e32a441cb292fb8abe6b0b38b
Lukas Balog <lukasbalog66@gmail.com>
2024-01-31 16:14:31 +0100

    generics support for fuzzy set

--- src/fuzzy_set.rs
+++ src/fuzzy_set.rs
@@ -1,27 +1,27 @@
 use std::ops::RangeInclusive;
 
 #[derive(Debug)]
-pub struct FuzzySet {
-    elements: Vec<(f32, f32)>,
+pub struct FuzzySet<T> {
+    elements: Vec<(T, f32)>,
     membership_interval: RangeInclusive<f32>,
 }
 
-impl FuzzySet {
-    pub fn new(elements: Vec<(f32, f32)>, membership_interval: Option<RangeInclusive<f32>>) -> FuzzySet {
+impl<T: PartialOrd + Copy> FuzzySet<T> {
+    pub fn new(elements: Vec<(T, f32)>, membership_interval: Option<RangeInclusive<f32>>) -> FuzzySet<T> {
         match membership_interval {
             Some(membership_interval) => FuzzySet {elements, membership_interval},
             None => FuzzySet{elements, membership_interval: (0.0..=1.0)},
         }
     }
 
-    pub fn add(&mut self, new_elem: (f32, f32)) {
+    pub fn add(&mut self, new_elem: (T, f32)) {
         let (elem, mem) = new_elem;
         
         if !self.is_membership_within_interval(mem) {
             panic!("{} is not in range {:?} of membership interval", mem, &self.membership_interval)
         }
 
-        for (el, mem) in &self.elements {
+        for (el, _) in &self.elements {
             if elem == *el {
                 return
             }
@@ -31,15 +31,15 @@ impl FuzzySet {
     }
 
 
-    pub fn remove(&mut self, elem: f32) {
-        if let Some(index) = self.elements.iter().position(|&(x, _)| x == elem) {
+    pub fn remove(&mut self, elem: T) {
+        if let Some(index) = self.elements.iter().position(|(x, _)| *x == elem) {
             self.elements.remove(index);
         } else {
-            panic!("element {} not in Fuzzy set", elem)
+            panic!("element not in Fuzzy set")
         }
     }
 
-    pub fn get_elem_membership(&self, elem: f32) -> Option<f32> {
+    pub fn get_elem_membership(&self, elem: T) -> Option<f32> {
         for (el, mem) in &self.elements {
             if elem == *el {
                 return Some(*mem)
@@ -48,7 +48,7 @@ impl FuzzySet {
         None
     }
 
-    pub fn change_elem_membership(&mut self, elem: f32, new_mem: f32) {
+    pub fn change_elem_membership(&mut self, elem: T, new_mem: f32) {
         for (el, mem) in self.elements.iter_mut() {
             if *el == elem {
                 *mem = new_mem;
@@ -56,12 +56,12 @@ impl FuzzySet {
         }
     }
 
-    pub fn union(&self, other: &FuzzySet) -> FuzzySet {
+    pub fn union(&self, other: &FuzzySet<T>) -> FuzzySet<T> {
         self.zadeh_operation(other, 
             |x: f32, y: f32| x.max(y))
     }
 
-    pub fn intersection(&self, other: &FuzzySet) -> FuzzySet {
+    pub fn intersection(&self, other: &FuzzySet<T>) -> FuzzySet<T> {
         self.zadeh_operation(other, 
             |x: f32, y: f32| x.min(y))
     }
@@ -75,14 +75,15 @@ impl FuzzySet {
     }
 }
 
-impl FuzzySet {
+impl<T: PartialOrd + Copy> FuzzySet<T> {
     fn is_membership_within_interval(&self, mem: f32) -> bool {
         self.membership_interval.contains(&mem)
     }
 
-    fn zadeh_operation<F>(&self, other: &FuzzySet, f: F) -> FuzzySet
-    where F: Fn(f32, f32) -> f32 {
-        let mut new_set = FuzzySet::new(vec![], None);
+    fn zadeh_operation<F>(&self, other: &FuzzySet<T>, f: F) -> FuzzySet<T>
+    where F: Fn(f32, f32) -> f32 { 
+        let elems: Vec<(T, f32)> = vec![];
+        let mut new_set = FuzzySet::new(elems, None);
 
         for val in &self.elements {
             new_set.elements.push(*val)
